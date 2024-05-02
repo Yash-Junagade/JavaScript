5f4581a7ac2cc55dd45f0b9a92826b1f247936e7
Yash Anil Junagade <yash.junagade1900@gmail.com>
2024-05-02 21:24:23 +0530

    Maths function

---
+++ ChaiaurCode/Part1/math.js
@@ -0,0 +1,14 @@
+// maths function in JS
+
+// console.log(Math.abs(-10));
+// console.log(Math.round(2.1));
+// console.log(Math.ceil(2.1));
+// console.log(Math.floor(2.9));
+// console.log(Math.sqrt(1000));
+
+// Math random
+// range for random
+
+let min = 10, max = 20;
+console.log(Math.floor(Math.random() * (max-min) + min) + 1);
+
