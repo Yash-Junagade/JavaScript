114ed73db9a910c1bee08e3d91e5724fa9b539d4
Yash Anil Junagade <yash.junagade1900@gmail.com>
2024-05-02 21:24:14 +0530

    Numbers in JS

---
+++ ChaiaurCode/Part1/numbers.js
@@ -0,0 +1,9 @@
+const num = 100
+console.log(num);
+
+const number = new Number(200);
+console.log(number);
+
+// there are many function in number
+console.log(number.toString().length);
+console.log(number.toFixed(4));
