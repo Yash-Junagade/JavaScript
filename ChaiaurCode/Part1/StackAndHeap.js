fa5388dcfc00d868566d6d6c67f878d7c0d62bc8
Yash Anil Junagade <yash.junagade1900@gmail.com>
2024-05-02 19:43:30 +0530

    Stack and Heap Memory Management

---
+++ ChaiaurCode/Part1/StackAndHeap.js
@@ -0,0 +1,29 @@
+/* 
+1. Premetive Data stored in the stack.
+2. Non Premetive Data store in the heap.
+*/
+
+// Premetive 
+
+let name = "yash";
+user = name;// value stored in the stack and give the copy of the original value
+user = "tony"; 
+
+// console.log(user);
+// console.log(name);
+
+
+// Non-Premetive
+
+let user1 = {
+    name : "yash",
+    email : "yash@gmail.com",
+    phNo : 8262879188
+};
+
+let user2 = user1; // it directly gives the refernce of the original object
+// it does not copy the object
+
+console.log(user1.name);
+user1.name = "tony";
+console.log(user2.name);
