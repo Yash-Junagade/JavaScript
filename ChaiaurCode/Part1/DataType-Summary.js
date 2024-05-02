e3b5c1bd083110aac498799659fe1458bac3e36d
Yash Anil Junagade <yash.junagade1900@gmail.com>
2024-05-02 19:43:02 +0530

    data type summery

---
+++ ChaiaurCode/Part1/DataType-Summary.js
@@ -0,0 +1,9 @@
+/* there are two types of data types in JS
+    this datatype is based on how the data is stored and how it get access 
+    
+    // Premitive Datatypes
+    Data Types :- String, Number, Boolean, Null, Undefined, BigInt, Symbol
+
+    // Reference Datatypes(non premitive)
+    Array, Object, Function
+ */
