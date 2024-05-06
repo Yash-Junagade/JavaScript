82899284cb287386dbc92a3852314b46a700ba34
Yash Anil Junagade <yash.junagade1900@gmail.com>
2024-05-06 09:31:39 +0530

    Complete the background color changer project using switch case

---
+++ ChaiaurCode/Part2/DOM_Projects/BgColor Changer/main.js
@@ -0,0 +1,24 @@
+const button = document.querySelectorAll(".button");
+const body = document.querySelector("body");
+
+button.forEach(function (btn) {
+    btn.addEventListener("click", function(e) {
+
+        switch(e.target.id){
+            case "grey":
+                body.style.background = "grey";
+                break;
+            case "white":
+                body.style.background = "white";
+                break;
+            case "blue":
+                body.style.background = "blue";
+                break;
+            case "yellow":
+                body.style.background = "yellow";
+                break;
+        }
+
+    })
+})
+
