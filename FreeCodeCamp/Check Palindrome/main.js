7e0d1636f5717240970fbdb617c7e89b77cfb05f
Yash Anil Junagade <yash.junagade1900@gmail.com>
2024-06-08 10:37:26 +0530

    added js for palindrome checker

--- FreeCodeCamp/Check Palindrome/main.js
+++ FreeCodeCamp/Check Palindrome/main.js
@@ -3,15 +3,17 @@ const checkBtn = document.getElementById("check-btn");
 const result = document.getElementById("result");
 
 checkBtn.addEventListener("click", () => {
-   if(!textInput.value){
-      alert("Please input a value");
-   } else {
-      let reverseStr = textInput.value.split("").reverse().join("");
-      console.log(reverseStr);
-      if(textInput.value === reverseStr){
-         result.innerHTML = `${textInput.value} is a palindrome`;
-      } else {
-         result.innerHTML = `${textInput.value} is not a palindrome`;
-      }
-   }
-})
+    if (!textInput.value.trim()) {
+        alert("Please input a value");
+    } else {
+        // Clean the input by removing non-alphanumeric characters and converting to lowercase
+        let cleanedInput = textInput.value.toLowerCase().replace(/[^a-z0-9]/g, '');
+        let reverseStr = cleanedInput.split("").reverse().join("");
+        console.log(reverseStr);
+        if (cleanedInput === reverseStr) {
+            result.innerHTML = `${textInput.value} is a palindrome`;
+        } else {
+            result.innerHTML = `${textInput.value} is not a palindrome`;
+        }
+    }
+});
